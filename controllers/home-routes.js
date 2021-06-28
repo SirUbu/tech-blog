afabd0b7afd607cb1b023b7edcabcc7f5f72429d
Sir Ubu <thesirubu@gmail.com>
2021-06-28 17:04:02 -0600

    Added single-blog page. Updated public js with logic.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -42,6 +42,47 @@ router.get('/', (req, res) => {
 });
 
 // route to get blog when comments clicked on
+router.get('/blog/:id', (req, res) => {
+    Blog.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'context',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM feel WHERE blog.id = feel.blog_id)'), 'feel_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'blog_id', 'blogger_id', 'created_at'],
+                include: {
+                    model: Blogger,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: Blogger,
+                attributes: ['username']
+            }
+        ]
+    }).then(dbBlogData => {
+        if (!dbBlogData) {
+            res.status(404).json({ message: 'There is no blog by that id.' });
+            return;
+        }
+        const blog = dbBlogData.get({ plain: true });
+        res.render('single-blog', {
+            blog,
+            loggedIn: req.session.loggedIn
+        });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // export routes
 module.exports = router;
